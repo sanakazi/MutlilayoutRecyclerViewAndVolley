c12f2ee1d573a6d260cccfc81fe94f9e5a5fbe6c
sanakazi <kazisana23@gmail.com>
2016-08-30 18:19:53 +0530

    Initial commit

--- app/src/main/java/com/example/sanakazi/recyclerandvolleyexampleproject/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/sanakazi/recyclerandvolleyexampleproject/adapters/RecyclerViewAdapter.java
@@ -18,7 +18,10 @@ import java.util.ArrayList;
  * Created by SanaKazi on 8/23/2016.
  */
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>  {
-
+    private static final int MALE = 0;
+    private static final int FEMALE = 1;
+    public String getGender;
+    View v;
 
     public interface OnItemClickListener {
         void onItemClick(ContactsJson.Contacts usersList);
@@ -34,30 +37,54 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.contact_list_row, parent, false);
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        MyViewHolder viewholder = null;
 
-        return new MyViewHolder(itemView);
+        Log.w("layout = ", viewType + " ");
+        switch (viewType) {
+            case MALE:
+                v = inflater.inflate(R.layout.contact_list_row, null);
+                viewholder = new MyViewHolder(v, viewType);
+                break;
+            case FEMALE:
+                v = inflater.inflate(R.layout.contact_list_row2, null);
+                viewholder = new MyViewHolder(v, viewType);
+                break;
+
+        }
+        return viewholder;
+
     }
+
+
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, final int position) {
 
         ContactsJson.Contacts contact = usersList.get(position);
-        holder.name.setText(contact.getName().toString());
-        holder.email.setText(contact.getEmail().toString());
-        holder.number.setText(contact.getPhone().getMobile().toString());
+        getGender= contact.getGender().toString();
+        Log.w("getGender= ", getGender);
+        if(getGender.equals("male"))
+        {
+            holder.name.setText(contact.getName().toString());
+            holder.email.setText(contact.getEmail().toString());
+            holder.number.setText(contact.getPhone().getMobile().toString());
 
-        Log.w("adapter name are " , contact.getName().toString() );
+            Log.w("adapter name are ", contact.getName().toString());
 
-        holder.name.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context," you clicked at position " + position , Toast.LENGTH_SHORT).show();
-            }
-        });
+        }
 
+        else if (getGender.equals("female"))
+        {
+            holder.name.setText(contact.getName().toString());
+            holder.email.setText(contact.getEmail().toString());
+            holder.number.setText(contact.getPhone().getMobile().toString());
+
+
+            Log.w("adapter name are ", contact.getName().toString());
+        }
 
     }
 
@@ -66,14 +93,38 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return usersList.size();
     }
 
+    //this method is called before calling onCreateViewHolder
+    @Override
+    public int getItemViewType(int position) {
+        if (usersList.get(position).getGender().equals("male")) {
+            return 0;
+
+        }else
+        {
+            return 1;
+        }
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView name, email, number;
 
-        public MyViewHolder(View view) {
-            super(view);
-            name = (TextView) view.findViewById(R.id.name);
-            email = (TextView) view.findViewById(R.id.email);
-            number = (TextView) view.findViewById(R.id.number);
+        public MyViewHolder(View itemView,int position) {
+            super(itemView);
+
+            if(position==MALE) {
+                name = (TextView) itemView.findViewById(R.id.name);
+                email = (TextView) itemView.findViewById(R.id.email);
+                number = (TextView) itemView.findViewById(R.id.number);
+            }
+            else if(position==FEMALE)
+            {
+                name = (TextView) itemView.findViewById(R.id.name);
+                email = (TextView) itemView.findViewById(R.id.email);
+                number = (TextView) itemView.findViewById(R.id.number);
+
+            }
         }
     }
+
+
 }
